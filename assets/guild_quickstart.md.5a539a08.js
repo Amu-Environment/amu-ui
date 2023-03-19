c8e6f1d8917625cddcf89c4ea3aa78495b21b77b
‘1942847253’ <1942847253@qq.com>
2023-03-19 19:18:30 +0800

    feat: push amu-ui docs

---
+++ assets/guild_quickstart.md.5a539a08.js
@@ -0,0 +1,10 @@
+import{_ as s,o as a,g as n,h as t}from"./app.557cbf82.js";const u=JSON.parse('{"title":"快速开始","description":"","frontmatter":{},"headers":[{"level":2,"title":"用法","slug":"用法","link":"#用法","children":[]}],"relativePath":"guild/quickstart.md"}'),e={name:"guild/quickstart.md"},l=t(`<h1 id="快速开始" tabindex="-1">快速开始 <a class="header-anchor" href="#快速开始" aria-hidden="true">#</a></h1><p>本节将介绍如何在项目中使用 Amu-UI</p><h2 id="用法" tabindex="-1">用法 <a class="header-anchor" href="#用法" aria-hidden="true">#</a></h2><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#A6ACCD;">&lt;template&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">  &lt;a-button&gt;按钮&lt;/a-button&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;/template&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;script setup&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">import { AButton } from &#39;amu-ui&#39;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;/script&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div>`,4),p=[l];function o(c,i,r,d,_,A){return a(),n("div",null,p)}const h=s(e,[["render",o]]);export{u as __pageData,h as default};
