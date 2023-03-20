1c6aafeb542a845031405931afc9eab312141384
‘1942847253’ <1942847253@qq.com>
2023-03-20 20:56:36 +0800

    feat: push amu-ui docs

---
+++ assets/guild_installation.md.44f5e62e.js
@@ -0,0 +1,11 @@
+import{_ as a,o as s,c as n,g as e}from"./app.b212b21a.js";const _=JSON.parse('{"title":"安装","description":"","frontmatter":{},"headers":[{"level":2,"title":"环境支持","slug":"环境支持","link":"#环境支持","children":[]},{"level":2,"title":"版本","slug":"版本","link":"#版本","children":[]},{"level":2,"title":"使用包管理器","slug":"使用包管理器","link":"#使用包管理器","children":[]}],"relativePath":"guild/installation.md"}'),l={name:"guild/installation.md"},p=e(`<h1 id="安装" tabindex="-1">安装 <a class="header-anchor" href="#安装" aria-hidden="true">#</a></h1><h2 id="环境支持" tabindex="-1">环境支持 <a class="header-anchor" href="#环境支持" aria-hidden="true">#</a></h2><p>由于 <code>Vue 3</code> 不再支持 IE11，<code>amu-ui</code> 也不再支持 IE11 浏览器。</p><h2 id="版本" tabindex="-1">版本 <a class="header-anchor" href="#版本" aria-hidden="true">#</a></h2><p>Amu-UI 正在持续开发中</p><h2 id="使用包管理器" tabindex="-1">使用包管理器 <a class="header-anchor" href="#使用包管理器" aria-hidden="true">#</a></h2><p>建议您使用包管理器 (NPM, Yarn, pnpm) 安装 Amu-UI</p><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#A6ACCD;"># 选择一个你喜欢的包管理器</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;"># NPM</span></span>
+<span class="line"><span style="color:#A6ACCD;">$ npm install amu-ui --save</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;"># Yarn</span></span>
+<span class="line"><span style="color:#A6ACCD;">$ yarn add amu-ui</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;"># pnpm</span></span>
+<span class="line"><span style="color:#A6ACCD;">$ pnpm install amu-ui</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div>`,8),t=[p];function i(c,o,r,d,h,u){return s(),n("div",null,t)}const C=a(l,[["render",i]]);export{_ as __pageData,C as default};
